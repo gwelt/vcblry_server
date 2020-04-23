edae4a7662af20a0676944b26c6558f47bdda233
sven <sven@gwelt.net>
2020-04-23 22:44:01 +0200

    do not .end with payload if code is set in arg1

--- utils/writer.js
+++ utils/writer.js
@@ -37,7 +37,7 @@ var writeJson = exports.writeJson = function(response, arg1, arg2) {
   }
   if(typeof payload === 'object') {
     payload = JSON.stringify(payload, null, 2);
-  }
+  } else {payload=undefined}
   response.writeHead(code, {'Content-Type': 'application/json'});
   response.end(payload);
 }
